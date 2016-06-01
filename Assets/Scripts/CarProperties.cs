c0bef5a20e0b01729328e3d550f4277ff0c25906
Kezada <Kezada94@gmail.com>
2016-05-31 23:42:31 -0400

    nuevsoadsd

---
+++ Assets/Scripts/CarProperties.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarProperties : MonoBehaviour{
+
+	public float maxSpeed;
+	public float acceleration;
+	public float weight;
+	public float health;
+	public float turnRate;
+	public float boost;
+	public float brakes;
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rg;
+	private CarProperties cp;
+
+	void Start () {
+		rg = GetComponent<Rigidbody2D> ();	
+		cp = GetComponent<CarProperties> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		if (v != 0f) {
+			doMovement (h, v);
+		}
+	}
+	void doMovement(float h, float v){
+		if (rg.velocity.magnitude < cp.maxSpeed) {
+			rg.AddForce (transform);
+		}
+	}	
+}
